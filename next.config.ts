460bcf9354953bf24953db26c32e45d2ba00b0a4
MichelPT <michelpt76767@gmail.com>
2025-06-19 09:41:04 +0700

    next.config.ts update

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
 
   // Your other configurations may be here
   // reactStrictMode: true,
