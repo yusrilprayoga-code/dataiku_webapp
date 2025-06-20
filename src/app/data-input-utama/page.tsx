e7fea6e17ecf9bcb4ade6b7954e6d8e448498c17
MichelPT <michelpt76767@gmail.com>
2025-06-20 15:20:44 +0700

    updated router for deploy

--- src/app/data-input-utama/page.tsx
+++ src/app/data-input-utama/page.tsx
@@ -1,5 +1,6 @@
 // app/data-input-utama/page.tsx
 /* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
 import React, { useState, useEffect } from 'react';
