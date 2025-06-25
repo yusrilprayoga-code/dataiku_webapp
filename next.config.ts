43e2553b346fef83a5d32cd72b0abe9c70957630
MichelPT <michelpt76767@gmail.com>
2025-06-26 06:19:13 +0700

    deleted next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://127.0.0.1:5001/api/:path*', // Proxy to your Flask app
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'http://127.0.0.1:5001/api/:path*', // Proxy to your Flask app
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
