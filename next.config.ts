7eda02344d8af10fc280d7ec780e26310ff8cfd9
yusrilprayoga-code <yusrilprayoga90@gmail.com>
2025-09-13 08:45:21 +0700

    Initial Commit

--- next.config.ts
+++ next.config.ts
@@ -6,21 +6,21 @@ const nextConfig = {
     // // Conditional asset prefix untuk deployment yang fleksibel
     // // Untuk development dan testing: tanpa prefix
     // // Untuk Dataiku: bisa menggunakan environment variable
-    // assetPrefix: process.env.ASSET_PREFIX || '',
+    assetPrefix: process.env.ASSET_PREFIX || '',
     
-    // // Untuk menghindari masalah dengan trailing slash di environment yang berbeda
-    // trailingSlash: true,
+    // Untuk menghindari masalah dengan trailing slash di environment yang berbeda
+    trailingSlash: true,
     
-    // // Pastikan gambar juga bekerja dengan path relatif
-    // images: {
-    //     unoptimized: true,
-    // },
+    // Pastikan gambar juga bekerja dengan path relatif
+    images: {
+        unoptimized: true,
+    },
     
-    // eslint: {
-    //     // Warning: This allows production builds to successfully complete even if
-    //     // your project has ESLint errors.
-    //     ignoreDuringBuilds: true,
-    // },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
     typescript: {
         // !! WARN !!
         // Dangerously allow production builds to successfully complete even if
